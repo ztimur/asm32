f624e27c6e77a94c2f5873196497756fe96a78ce
Timur Zhamakeev <ztimur@gmail.com>
2017-12-02 09:16:26 +0600

    sumofsquares cleaned and fixed

--- sumofsquares/sumofsquares.asm
+++ sumofsquares/sumofsquares.asm
@@ -7,17 +7,20 @@ section .text
 sumofsquares:
 	push	ebp
 	mov	ebp, esp
+	push	ebx
 ;
 	mov	ecx, dword [ebp+8]
 	mov	ebx, 1
 label1:
 	mov	eax, ebx
 	mul	eax
+	add	dword [sumOfSquares], eax
 	inc	ebx
 	loop	label1
 
 	mov	eax, dword [sumOfSquares]
 ;
+	pop	ebx
 	mov	esp, ebp
 	pop	ebp
 	ret			; возврат
--- sumofsquares/sumofsquares10.asm
+++ sumofsquares/sumofsquares10.asm
@@ -8,17 +8,20 @@ section .text
 sumofsquares10:
 	push	ebp
 	mov	ebp, esp
+	push	ebx
 ;
 	mov	ecx, dword [n]
 	mov	ebx, 1
 label1:
 	mov	eax, ebx
 	mul	eax
+	add	dword [sumOfSquares], eax
 	inc	ebx
 	loop	label1
 
 	mov	eax, dword [sumOfSquares]
 ;
+	pop	ebx
 	mov	esp, ebp
 	pop	ebp
 	ret			; возврат
